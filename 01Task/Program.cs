fb9a105ff70f121354b3381794c9574ca0c45122
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-17 10:48:25 +0300

    Программа выдает заполненный массив на экран. Элементы заполняются генератором случайных чисел.

---
+++ 01Task/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+int m = new Random().Next(2,6);
+int n = new Random().Next(2,6);
+Console.WriteLine($"{m} {n}");
+int[,] Array = new int[m,n];
+for(int i = 0; i<Array.GetLength(0);i++)
+{
+    for(int j = 0; j<Array.GetLength(1); j++)
+    {
+        Array[i,j] = new Random().Next(0,11);
+        Console.Write($"{Array[i,j]} ");
+    }
+    Console.WriteLine();
+}
