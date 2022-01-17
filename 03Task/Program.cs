26d2dbb5eda78d0335b02a6c6426823f555106f5
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-17 12:32:05 +0300

    Программа реализована двумя методами. Один метод запоняет двумерный массив по правилу A[m,n] = m+n. А второй метод выводит содержимое на экран. Столбцы и строки генерируются в пером методе с помощью генератора случайных чисел.

---
+++ 03Task/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int[,] FillArray()
+{
+    Random random = new Random();
+    int row = random.Next(2,11);
+    int column = random.Next(2,11);
+    int[,] Array = new int[row, column];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = i+j;
+        }
+    }
+    return Array;
+}
+void ShowArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] Array = FillArray();
+ShowArray(Array);
