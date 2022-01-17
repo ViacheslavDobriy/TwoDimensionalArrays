e3396c722e95fb42bc929f93df4e33b6ea58dfa6
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-17 11:00:49 +0300

    Начал добавлять функции

--- 01Task/Program.cs
+++ 01Task/Program.cs
@@ -1,5 +1,18 @@
 ﻿// Показать двумерный массив размером m×n заполненный целыми числами
 
+/*int[,] CreateMatrix(uint rows, uint columns, int start, int end)
+{
+    Random random = new Random();
+    int[,] matrix = new int[rows, columns];
+    for(int i =0; i<rows;i++)
+        for(int j = 0; j<columns; j++)
+        matrix[i,j] = random.Next(start,end);
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+
+}*/
 int m = new Random().Next(2,6);
 int n = new Random().Next(2,6);
 Console.WriteLine($"{m} {n}");
