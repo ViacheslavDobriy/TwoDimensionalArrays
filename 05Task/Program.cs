da4bfbe195e05c776d272bc619c223701dcbd7bb
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-18 10:08:47 +0300

    5 Задача. Программа генерирует двумерный массив и запрашивает пользователя ввести число, чтобы найти его и показать где оно встречается или опровегнуть утверждение о том, что введенное число есть в массиве

--- 05Task/Program.cs
+++ 05Task/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет
+
+int[,] FillArray()
+{
+    Random random =new Random();
+    int[,] Array = new int[random.Next(1,6), random.Next(1,6)];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return Array;
+}
+void ShowArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FoundStuff(int[,] Array)
+{
+    Console.Write("Введите число, которое нужно найти: ");
+    int UserNumber = Convert.ToInt32(Console.ReadLine());
+    bool IsItHere = false;
+    Console.WriteLine();
+    for(int i =0;i<Array.GetLength(0);i++)
+    {
+        for(int j = 0; j < Array.GetLength(1);j++)
+        {
+            if(Array[i,j] == UserNumber)
+            {
+                Console.WriteLine("Ваше число есть в {0}-ой строке в {1}-ом столбце",i+1,j+1);
+                IsItHere = true;
+            }
+        }
+    }
+    if(!IsItHere)  Console.WriteLine("Ваше число ни разу не встречается в данном двумерном массиве");
+}
+int[,] OurArray = FillArray();
+ShowArray(OurArray);
+FoundStuff(OurArray);
