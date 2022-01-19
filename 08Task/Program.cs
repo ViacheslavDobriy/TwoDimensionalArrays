da95716368c14d9ea19ec1108f314cfef894e02f
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-19 11:08:46 +0300

    программа меняет первый ряд с последним в случайно заданном двумерном массиве

---
+++ 08Task/Program.cs
@@ -0,0 +1,45 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] FillArray()
+{
+    Random random = new Random();
+    int row = random.Next(2,11);
+    int column = random.Next(2,11);
+    int[,] Array = new int[row, column];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = random.Next(0,31);
+        }
+    }
+    return Array;
+}
+void ShowArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] ChangeFirstAndLastRows(int[,] Array)
+{
+    int[] extraRow = new int[Array.GetLength(1)];
+    int rows = Array.GetLength(0);
+    for(int i=0; i<Array.GetLength(1);i++)
+    {
+        extraRow[i] = Array[0,i];
+        Array[0,i] = Array[rows-1,i];
+        Array[rows-1,i] = extraRow[i];
+    }
+    return Array;
+}
+int[,] RandomArray = FillArray();
+ShowArray(RandomArray);
+Console.WriteLine("Количество рядов - {0}; Количество столбцов - {1}",RandomArray.GetLength(0),RandomArray.GetLength(1));
+RandomArray= ChangeFirstAndLastRows(RandomArray);
+ShowArray(RandomArray);
